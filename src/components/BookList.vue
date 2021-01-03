429be0474960ffa909c224ca13e4d052284e05e9
tuurb <38724175+tuurb@users.noreply.github.com>
2021-01-03 17:21:26 +0100

    Module 2 finished

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
   <h1>{{title}}</h1>
+  <ul>
+    <li v-for="book in books" :key='book.id'>
+      {{book.title}}: {{book.author}}
+    </li>
+  </ul>
 </div>
 
 </template>
@@ -10,14 +15,27 @@ export default {
   name: 'BookList',
   data() {
     return {
-      title: 'All Books'
+      title: 'All Books',
+      books: [
+        {id: 1, title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+        {id: 2, title: 'American Gods', author: 'Neil Gaiman'},
+        {id: 3, title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+      ],
     };
-  }
+  },
 }
 </script>
 
 <style>
 h1, h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
